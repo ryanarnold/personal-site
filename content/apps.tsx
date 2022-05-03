11a71d855336a8a2ed4b78088257155df669a308
Ryan <ryan@geekful.dev>
2022-05-03 14:32:57 +0800

    Display apps

---
+++ content/apps.tsx
@@ -0,0 +1,15 @@
+const apps = [
+  {
+    title: 'Baybayin Quiz App',
+    description:
+      'A web app for helping to learn the native alphabet of the Philippines - Baybayin.',
+    madewith: ['Svelte'],
+  },
+  {
+    title: 'Sulyap Balita',
+    description: 'View summarized daily news',
+    madewith: ['React', 'Next.js'],
+  },
+];
+
+export default apps;
--- pages/apps/index.tsx
+++ pages/apps/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import apps from '../../content/apps';
 
 interface Props {}
 
 function AppsPage({}: Props) {
-  return <div>AppsPage</div>;
+  return (
+    <div>
+      <p>These are some of the apps I&apos;ve made. Enjoy!</p>
+      {apps.map((app) => (
+        <div>
+          <h3>{app.title}</h3>
+          <p>{app.description}</p>
+          <p>
+            Made with{' '}
+            {app.madewith.map((tech, index) => {
+              if (index === 0) return <span>{tech}</span>;
+              return <span>, {tech}</span>;
+            })}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default AppsPage;
